77334280494991671eaacceeae064367aa5ab4e2
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-05-19 19:51:57 +0300

    added old_balance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        int old_balance = 200;
         int balance_pay = 100;
         if (balance_pay > 1000) {
             int bonus = balance_pay / 100;
@@ -7,13 +8,13 @@ public class Main {
             System.out.println();
             System.out.println("Бонус " + bonus + " рублей");
             System.out.println();
-            System.out.println("Итоговый баланс " + (balance_pay + bonus) + " рублей");
+            System.out.println("Итоговый баланс " + (old_balance + balance_pay + bonus) + " рублей");
         } else {
             System.out.println("Сумма пополнения " + balance_pay + " рублей");
             System.out.println();
             System.out.println("Бонус 0 рублей" );
             System.out.println();
-            System.out.println("Итоговый баланс " + balance_pay + " рублей");
+            System.out.println("Итоговый баланс " + (old_balance + balance_pay) + " рублей");
         }
     }
 }
